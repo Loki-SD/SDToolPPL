29a8e479b3b32f09da9efeb0dcb8cc00a7919732
Loki-SD <michaelloquias96@gmail.com>
2022-10-03 20:34:11 +0800

    first commit

--- src/test/java/ph/newtoolppl/toolppl/ToolPplApplicationTests.java
+++ src/test/java/ph/newtoolppl/toolppl/ToolPplApplicationTests.java
@@ -8,6 +8,50 @@ class ToolPplApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("Hello World");
+
+        int n, i, j, space = 0;
+       /* System.out.print("Enter the number of rows: ");*/
+        n = 5;
+        space = n - 1;
+        for (j = 1; j <= n; j++) {
+
+            for (i = 1; i <= space; i++) {
+
+                System.out.print(" ");
+            }
+
+            space--;
+
+            for (i = 1; i <= 2 * j - 1; i++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println("");
+        }
+
+        space = 1;
+
+        for (j = 1; j <= n - 1; j++) {
+
+            for (i = 1; i <= space; i++) {
+
+                System.out.print(" ");
+
+            }
+
+            space++;
+
+            for (i = 1; i <= 2 * (n - j) - 1; i++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println("");
+        }
     }
 
 }
