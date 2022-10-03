dc66ff729ce4af3e57351e85c8786b03c44000d9
Loki-SD <michaelloquias96@gmail.com>
2022-10-03 20:17:04 +0800

    first commit

---
+++ src/main/java/ph/newtoolppl/toolppl/ToolPplApplication.java
@@ -0,0 +1,13 @@
+package ph.newtoolppl.toolppl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolPplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolPplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ph/newtoolppl/toolppl/ToolPplApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.newtoolppl.toolppl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolPplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
